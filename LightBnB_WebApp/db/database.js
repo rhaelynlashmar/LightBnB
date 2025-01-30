29d61693b7791df478e838c96a7d1c12dd35820c
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-01-30 13:59:56 -0800

    REFACTOR- altered getAllReservation function to a SQL query

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -94,11 +94,28 @@ const addUser = function(user) {
 
 /**
  * Get all reservations for a single user.
+ * @param {number} limit The number of result to return
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT reservations.*, properties.*, users.*
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN users ON properties.owner_id = users.id
+  WHERE reservations.guest_id = $1
+  LIMIT $2;
+`;
+const values = [guest_id, limit];
+
+return pool
+  .query(queryString, values)
+  .then(res => res.rows)
+  .catch(err => {
+    console.error('query error', err.stack);
+    return null;
+  });
 };
 
 /// Properties
